9b03e90a176741d620f9952aad319e27c7088160
Immanuel <immanuel.peter1@gmail.com>
2025-08-01 22:49:10 -0500

    Update __init__.py

--- models/experts/__init__.py
+++ models/experts/__init__.py
@@ -2,5 +2,6 @@ from .bdd_detection_expert import BDDDetectionExpert
 from .bdd_drivable_expert import BDDDrivableExpert
 from .bdd_segmentation_expert import BDDSegmentationExpert
 from .nuscenes_expert import NuScenesExpert
+from .carla_expert import CarlaExpert
 
-__all__ = ["BDDDetectionExpert", "BDDDrivableExpert", "BDDSegmentationExpert", "NuScenesExpert"]
+__all__ = ["BDDDetectionExpert", "BDDDrivableExpert", "BDDSegmentationExpert", "NuScenesExpert", "CarlaExpert"]
